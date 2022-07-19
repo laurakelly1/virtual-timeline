814914dfcaf3e0acbc37e9fb0e45d0f79dae712c
laurakelly1 <lkelly.laura@gmail.com>
2022-07-19 10:05:42 -0400

    Deploying

--- virtual_timeline_app/settings.py
+++ virtual_timeline_app/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'virtual_timeline_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': s.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
